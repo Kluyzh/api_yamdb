f2e1d7bd95f10ad544a9215770f961ba1080d974
Ilya Kluyzhev <ilia.kluyzhev.2498@gmail.com>
2025-04-30 12:14:03 +0300

    Add Review and Comment models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Review(models.Model):
+    title = models.IntegerField()
+    # title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='reviews')
+    author = models.IntegerField()
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    score = models.PositiveSmallIntegerField(
+        validators=(
+            MinValueValidator(1, 'Score can\'t be lesser than 1'),
+            MaxValueValidator(10, 'Score can\'t be bigger than 10'),
+        )
+    )
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    review = models.IntegerField()
+    # review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='comments')
+    author = models.IntegerField()
+    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
